25c1bfd0f0b19f85e83ee1c78bb74e247d2f55fe
Hanan <hshuaib@qatar.cmu.edu>
2016-03-18 14:54:01 +0300

    initial applicant form

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,5 +1,15 @@
 class Applicant < ActiveRecord::Base
+
 has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+
+NATIONALITY_LIST = ["Qatari", "nonQatari"]
+
+SEX_LIST = ["ذكر", "أنثى"]
+
+STATUS_LIST = ["أعزب", "متزوج"]
+
+EDUCATION_LIST = ["لا يوجد","إبتدائي","إعدادي","ثانوي","جامعي"]
 
 end
